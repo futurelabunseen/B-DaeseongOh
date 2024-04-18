8b5670da499264989d86183ec93a698ee76ad2f5
gnoesead <abr00122@naver.com>
2024-04-19 02:17:30 +0900

    서브시스템에 오브젝트 풀 만들기

---
+++ MakeDungeon/Source/MakeDungeon/Private/Game/MDCartridgeGameInstanceSubsystem.cpp
@@ -0,0 +1,8 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Game/MDCartridgeGameInstanceSubsystem.h"
+
+UMDCartridgeGameInstanceSubsystem::UMDCartridgeGameInstanceSubsystem()
+{
+}
---
+++ MakeDungeon/Source/MakeDungeon/Public/Game/MDCartridgeGameInstanceSubsystem.h
@@ -0,0 +1,21 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/GameInstanceSubsystem.h"
+#include "MDCartridgeGameInstanceSubsystem.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MAKEDUNGEON_API UMDCartridgeGameInstanceSubsystem : public UGameInstanceSubsystem
+{
+	GENERATED_BODY()
+	
+public:
+	UMDCartridgeGameInstanceSubsystem();
+
+
+};
